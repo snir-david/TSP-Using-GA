eddd79e962db17055f0c2f488d7b19749d99ee7b
snir-david <snirdavid1994@gmail.com>
2022-04-11 18:01:40 +0300

    crossover function

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
+import random
+
 from main import CITIES
+import numpy as np
 
 
 class Individual:
@@ -9,16 +12,19 @@ class Individual:
 class Population:
     def __init__(self, size):
         self.populationSize = size
-        pass
+        self.population = []
+        for i in range(size):
+            c = Chromosome()
+            c.random_init()
+            self.population.append(c)
 
 
 class Chromosome:
-    def __init__(self, route):
-        self.route = route
-        pass
+    def __init__(self):
+        self.route = []
 
     def random_init(self):
-        cities = CITIES
+        self.route = random.sample(CITIES, len(CITIES))
 
 
 class Individual:
--- main.py
+++ main.py
@@ -1,9 +1,41 @@
+import random
+import numpy as np
+import classes
+
 POPULATION_SIZE = 10
-CITIES = ['A', 'B', 'C', 'D', 'E']
+CITIES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
-def crossover(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def crossover(c1, c2):
+    offspring1 = [None] * len(CITIES)
+    offspring2 = [None] * len(CITIES)
+    offspring1_idx = 0
+    offspring2_idx = 0
+    idx1 = random.randint(0, len(CITIES)-1)
+    idx2 = random.randint(idx1, len(CITIES))
+    diff = idx2 - idx1
+    for i in range(idx1, idx2):
+        offspring1[i] = c1.route[i]
+        offspring2[i] = c2.route[i]
+    for j in range(len(CITIES)):
+        if c2.route[j] not in offspring1 and ((offspring1_idx + diff) < len(CITIES)):
+            if j < idx1:
+                offspring1[offspring1_idx] = c2.route[j]
+            else:
+                offspring1[offspring1_idx + diff] = c2.route[j]
+            offspring1_idx += 1
+        if c1.route[j] not in offspring2 and ((offspring2_idx + diff) < len(CITIES)):
+            if j < idx1:
+                offspring2[offspring2_idx] = c1.route[j]
+            else:
+                offspring2[offspring2_idx + diff] = c1.route[j]
+            offspring2_idx += 1
+    return offspring1, offspring2
+
+
+# {ABCED}
+# {BDECA}
+# {XX
 
 
 def mutation(name):
@@ -23,4 +55,7 @@ def init_population(name):
 
 
 if __name__ == '__main__':
-    crossover('PyCharm')
+    p = classes.Population(10)
+    for i in p.population:
+        print(i.route)
+    off1, off2 = crossover(p.population[0], p.population[1])

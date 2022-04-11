b1bfc4518f01fc621e08d34875a7a0e921ad14fa
snir-david <snirdavid1994@gmail.com>
2022-04-11 09:58:51 +0300

    starting over

---
+++ classes.py
@@ -0,0 +1,26 @@
+from main import CITIES
+
+
+class Individual:
+    def __init__(self):
+        pass
+
+
+class Population:
+    def __init__(self, size):
+        self.populationSize = size
+        pass
+
+
+class Chromosome:
+    def __init__(self, route):
+        self.route = route
+        pass
+
+    def random_init(self):
+        cities = CITIES
+
+
+class Individual:
+    def __init__(self):
+        pass
---
+++ main.py
@@ -0,0 +1,26 @@
+POPULATION_SIZE = 10
+CITIES = ['A', 'B', 'C', 'D', 'E']
+
+
+def crossover(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def mutation(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def selection(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def fittness(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def init_population(name):
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    crossover('PyCharm')
